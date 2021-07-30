ab9f09891123a16c0a8903b89f5e0ec220dc5d3b
Abraham Serrano <abrahamserrano96@hotmail.com>
2021-07-29 20:49:38 -0500

    add(components): add pages with url dynamic and components into the markdown

---
+++ components/Podcast.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Podcast({ episode }) {
+    return (
+        <iframe
+            src={`https://open.spotify.com/embed/episode/${episode}`}
+            width="100%"
+            height="232"
+            frameBorder="0"
+            allowTransparency="true"
+            allow="encrypted-media"
+        ></iframe>
+    );
+}
